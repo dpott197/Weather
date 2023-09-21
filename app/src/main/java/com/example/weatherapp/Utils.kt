a872a6251c747005d6ecf810cf7e78d59faaedaf
Darren Pottinger <darren.pottinger@gmail.com>
2023-09-21 17:15:25 -0400

    Update Utils.kt

--- app/src/main/java/com/example/weatherapp/Utils.kt
+++ app/src/main/java/com/example/weatherapp/Utils.kt
@@ -1,18 +1,11 @@
 package com.example.weatherapp
 
 class Utils {
-    companion object{
+    companion object {
+        var BASE_URL: String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY: String = "4ebff39a22d11a664acf3ce64f03b726"
 
-        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
-        var API_KEY  : String = "insert your own key"
-         const val PERMISSION_REQUEST_CODE = 123
-
-         const val LOCATION_PERMISSION_REQUEST_CODE = 1001
-
-
-
-
-
-
+        const val PERMISSION_REQUEST_CODE = 123
+        const val LOCATION_PERMISSION_REQUEST_CODE = 1001
     }
 }
